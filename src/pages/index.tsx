5bbe8a2879397f3fe14bea10f33cf32c6673dd82
Vahagn Aharonian <vahagn.aharonian@gmail.com>
2021-07-29 18:17:03 +0400

    Initial Gatsby project setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Box, Text } from "@chakra-ui/react";
+
+const IndexPage = () => {
+  return (
+    <Box>
+      <Text>Setup</Text>
+    </Box>
+  );
+};
+
+export default IndexPage;
